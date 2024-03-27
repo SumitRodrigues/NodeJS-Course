5e4ac94822a8661528789848ac70aa0d5b31c08e
SumitRodrigues <sumitrod11@gmail.com>
2024-03-27 10:40:15 -0700

    Third Commit

--- notes-app/app.js
+++ notes-app/app.js
@@ -1,10 +1,18 @@
-const fs = require('fs')
+// #1) Importing NodeJS core modules
+// const fs = require('fs')
 
-fs.writeFileSync('notes.txt', 'This file was created by node.js!')
+// fs.writeFileSync('notes.txt', 'This file was created by node.js!')
 
-//Challenge: Append a message to notes.txt
-// 1. Use appendFileSync to append to the file
-// 2. Run the script
-// 3. Check your work by opening the file and viewing the appended text
+// //Challenge: Append a message to notes.txt
+// // 1. Use appendFileSync to append to the file
+// // 2. Run the script
+// // 3. Check your work by opening the file and viewing the appended text
 
-fs.appendFileSync('notes.txt', 'This data was appended using appendFileSync!')
+// fs.appendFileSync('notes.txt', ' I live in California!')
+
+// #2) Importing your own files
+
+const name = require('./utils.js')
+
+
+console.log(name)
---
+++ notes-app/utils.js
@@ -0,0 +1,5 @@
+console.log('utils.js')
+
+const name = 'Rodz'
+
+module.exports = name
