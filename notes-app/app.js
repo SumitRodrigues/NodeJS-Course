8e0ccc62005fc7a51e4ead1e3829137704e48441
SumitRodrigues <sumitrod11@gmail.com>
2024-03-27 09:47:10 -0700

    second commit

--- notes-app/app.js
+++ notes-app/app.js
@@ -1,1 +1,10 @@
-FileSystem.writeFileSync('notes.txt', 'This file was created by node.js!')
+const fs = require('fs')
+
+fs.writeFileSync('notes.txt', 'This file was created by node.js!')
+
+//Challenge: Append a message to notes.txt
+// 1. Use appendFileSync to append to the file
+// 2. Run the script
+// 3. Check your work by opening the file and viewing the appended text
+
+fs.appendFileSync('notes.txt', 'This data was appended using appendFileSync!')
