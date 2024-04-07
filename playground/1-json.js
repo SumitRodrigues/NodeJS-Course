23f00d83994451d39fec40f613cd3c5a1c460e71
SumitRodrigues <sumitrod11@gmail.com>
2024-04-06 23:49:22 -0700

    created a new json file from js file

--- playground/1-json.js
+++ playground/1-json.js
@@ -1,7 +1,8 @@
+const fs = require('fs')
 const book = {
     title: 'Ego is the Enemy',
     author: 'Ryan Holiday'
 }
 
 const bookJSON = JSON.stringify(book) // "{"title":"Ego is the Enemy","author":"Ryan Holiday"}"
-console.log(bookJSON)
+fs.writeFileSync('1-json.json', bookJSON)
