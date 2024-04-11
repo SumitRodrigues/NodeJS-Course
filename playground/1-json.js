96dfcc0d02018f38b64ea99a5a6f3f65182ded53
SumitRodrigues <sumitrod11@gmail.com>
2024-04-11 16:39:41 -0700

    challenge start

--- playground/1-json.js
+++ playground/1-json.js
@@ -1,8 +1,23 @@
 const fs = require('fs')
-const book = {
-    title: 'Ego is the Enemy',
-    author: 'Ryan Holiday'
-}
+// const book = {
+//     title: 'Ego is the Enemy',
+//     author: 'Ryan Holiday'
+// }
 
-const bookJSON = JSON.stringify(book) // "{"title":"Ego is the Enemy","author":"Ryan Holiday"}"
-fs.writeFileSync('1-json.json', bookJSON)
+// const bookJSON = JSON.stringify(book) // "{"title":"Ego is the Enemy","author":"Ryan Holiday"}"
+// fs.writeFileSync('1-json.json', bookJSON)
+
+// const dataBuffer = fs.readFileSync('1-json.json')
+// const dataJSON = dataBuffer.toString()
+// const data = JSON.parse(dataJSON)
+// console.log(data.title)
+
+
+// Challenge: Work with JSON and the file syatem
+// 1. Load and parse the JSON data
+// 2. Change the name and age property using your info
+// 3. Stringify the changed object and overwrite the original data
+// 4. Test your work by viewing data in the JSON file
+
+
+
